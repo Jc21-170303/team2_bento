05a54639f3214798aad59a0f6eb55e66d7ca5ea4
170112 <170112@e0505-19.JC-21.local>
2019-11-08 12:21:35 +0900

    テスト

---
+++ team2_Bento/src/jp/ac/jc21/jk3a34/bento01.java
@@ -0,0 +1,33 @@
+package jp.ac.jc21.jk3a34;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class bento01
+ */
+@WebServlet("/bento01")
+public class bento01 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public bento01() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
